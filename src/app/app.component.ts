29ad7bb1e8bc4c89a0294266035dc7f751bee522
Lewis Kirkaldie <lewis@cinegy.com>
2018-01-26 09:56:48 +0000

    -added more tests for javascript bridges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,14 +15,35 @@ export class AppComponent {
   constructor() {
     this.currentMcrItem = new McrItem();
     
-    this.currentMcrItem.Name = "Item Default Name";
+    this.currentMcrItem.Name = "Item Name";
     this.currentMcrItem.Comment = "Item Default Comment";
 
+    if(this.currentMcrItem.Name == "jjj")
+    {
+      this.SelectedItemUpdated();
+    }
+
+  }
+
+  
+
+  public SelectedItemUpdated() : void
+  {
+    this.currentMcrItem.Name = "Fired!";
   }
   
   public playItem(item:McrItem) : boolean
   {
-    item.Name = `Clicked at ${new Date().getTime()}`;
+    var wang = window.external["Numberwang"];
+    item.Name = `C# apps say numberwang is: ${wang}`;
+    
+    return false;
+  }
+
+  public clearItem(item:McrItem) : boolean
+  {
+    window.external["Test"]("Current item name: " + item.Name);
+    
     return false;
   }
 }
